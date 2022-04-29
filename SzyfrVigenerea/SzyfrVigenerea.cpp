836209bbaa0bb2d198b6eae28395350ac9032de8
Krzysztof Krakowiak <komumor@wp.pl>
2022-04-29 09:07:37 +0200

    dodanie funkcji deszyfrującej

--- SzyfrVigenerea/SzyfrVigenerea.cpp
+++ SzyfrVigenerea/SzyfrVigenerea.cpp
@@ -24,7 +24,25 @@ string encryption(string text, string key) {
 
 //funkcja do odszyfrowania, argumenty to tekst do odszyfrowania i klucz szyfru, zwraca odszyfrowany tekst
 string decryption(string text, string key) {
-   
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] >= 65 && text[i] <= 90) { // duże litery
+            for (int j = 0; j < 26; j++) {
+                if (tabUppercase[j][key[i] - 65] == text[i]) {
+                    text[i] = j + 65;
+                    break;
+                }
+            }
+            
+        }
+        else if (text[i] >= 97 && text[i] <= 122) { // małe litery 
+            for (int j = 0; j < 26; j++) {
+                if (tabLowercase[j][key[i] - 97] == text[i]) {
+                    text[i] = j + 97;
+                    break;
+                }
+            }
+        }
+    }
 
     return text;
 }
@@ -35,6 +53,7 @@ int main()
     int option, pom;
     string text, key;
 
+    //zapełnianie tablic literami
     for (int i = 0; i < 26; i++) {
         for (int j = 0; j < 26; j++) {
             pom = j + i + 97;
@@ -63,7 +82,7 @@ int main()
 
         do {
             cin >> option;
-            if (option == 1) {
+            if (option == 1) {                                       //szyfrowanie
                 cout << "Wybrno szyfrowanie\n";  
                 cout << "\nPodaj tekst do zaszyfrowania:\n";
                 getline(cin >> ws, text);
@@ -72,8 +91,13 @@ int main()
                 text = encryption(text, key);
             }
 
-            else if (option == 2) {
+            else if (option == 2) {                                  //deszyfrowanie
                 cout << "Wybrano deszyfrowanie\n";
+                cout << "\nPodaj tekst do odszyfrowania:\n";
+                getline(cin >> ws, text);
+                cout << "\nPodaj klucz szufru:\n";
+                getline(cin >> ws, key);
+                text = decryption(text, key);
             }
 
             else if (option == 3) {
